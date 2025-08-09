478f5c60e5911dc8f09a0a2198760fc5de34fb21
Nayan <nayanunni95@gmail.com>
2025-08-09 13:29:51 +0530

    fic(vercel)

--- run.py
+++ run.py
@@ -1,6 +1,10 @@
 import os
 import uvicorn
 
+# Import the FastAPI app so that Vercel can detect it
+from app.main import app
+
 if __name__ == "__main__":
+    # Local & Railway execution
     port = int(os.environ.get("PORT", 8000))
-    uvicorn.run("app.main:app", host="0.0.0.0", port=port)
+    uvicorn.run("run:app", host="0.0.0.0", port=port)
