df531ddd4dadece46732c69e7a2300845b37836b
Nayan <nayanunni95@gmail.com>
2025-08-09 12:48:25 +0530

    fix()

--- run.py
+++ run.py
@@ -1,4 +1,6 @@
-if __name__ == "__main__":
-    import uvicorn
+import os
+import uvicorn
 
-    uvicorn.run("app.main:app", host="127.0.0.1", port=8004, reload=True)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run("app.main:app", host="0.0.0.0", port=port)
