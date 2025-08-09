ece7d221367ba9b45fc6104ada2bf1c5b4fc9e4c
Nayan <nayanunni95@gmail.com>
2025-08-09 06:36:39 +0530

    refactor()

--- app/api/endpoints/manage_auth.py
+++ app/api/endpoints/manage_auth.py
@@ -16,33 +16,33 @@ from app.core.config import settings
 router = APIRouter(route_class=ExceptionLoggingRoute)
 
 
-@router.post('/register/')
-async def register(request: Request, db: SessionDep):
-    try:
-        data = FormUtil.form_data_to_json(await request.form())
-    except MultipartParseError:
-        data = {}
-    email = data.get('email')
-    password = data.get('password')
+# @router.post('/register/')
+# async def register(request: Request, db: SessionDep):
+    # try:
+    #     data = FormUtil.form_data_to_json(await request.form())
+    # except MultipartParseError:
+    #     data = {}
+    # email = data.get('email')
+    # password = data.get('password')
 
-    if (await db.scalar(select(User).filter(User.email == email))):
-        return CustomResponse(general_message="Email already exist.").get_failure_response()
+    # if (await db.scalar(select(User).filter(User.email == email))):
+    #     return CustomResponse(general_message="Email already exist.").get_failure_response()
 
-    user = User(name=email.split('@')[0], email=email, password=Hash.encrypt_password(password) if password else None,  role=Roles.VIEWER.value)
-    user_id = user.id
-    user_email = user.email
-    user_name = user.name
-    user_role = user.role
-    db.add(user)
-    await db.commit()
-    return CustomResponse(
-        general_message="User successfully registered.",
-        response={
-            "id": user_id,
-            "name": user_name,
-            "email": user_email,
-            "role": user_role
-    }).get_success_response()
+    # user = User(name=email.split('@')[0], email=email, password=Hash.encrypt_password(password) if password else None,  role=Roles.VIEWER.value)
+    # user_id = user.id
+    # user_email = user.email
+    # user_name = user.name
+    # user_role = user.role
+    # db.add(user)
+    # await db.commit()
+    # return CustomResponse(
+    #     general_message="User successfully registered.",
+    #     response={
+    #         "id": user_id,
+    #         "name": user_name,
+    #         "email": user_email,
+    #         "role": user_role
+    # }).get_success_response()
 
 
 @router.post('/login/')
@@ -53,19 +53,46 @@ async def login(request: Request, db: SessionDep):
         data = {}
     email = data.get('email')
     password = data.get('password')
-    otp = data.get('otp')
+    # otp = data.get('otp')
 
     if not (user := (await db.scalar(select(User).filter(User.email == email)))):
-        return CustomResponse(general_message="User not found.").get_failure_response()
+        if not (name := data.get('name')):
+            return CustomResponse(message="name field is required.").get_failure_response()
+
+        if not (password := data.get('password')):
+            return CustomResponse(message="password field is required.").get_failure_response()
+
+        user = User(name=name, email=email, password=Hash.encrypt_password(password) if password else None,  role=Roles.USER.value)
+        user_id = user.id
+        user_email = user.email
+        user_name = user.name
+        user_role = user.role
+        user_access_token = JWTUtil.create_access_token({"id": user_id})
+        user_refresh_token = JWTUtil.create_refresh_token({"id": user_id})
+
+        db.add(user)
+        await db.commit()
+        return CustomResponse(
+        general_message="User successfully registered.",
+        response={
+            "id": user_id,
+            "name": user_name,
+            "email": user_email,
+            "role": user_role,
+            "access_token_expiry": settings.ACCESS_TOKEN_EXPIRE_MINUTES,
+            "access_token": user_access_token,
+            "refresh_token_expiry": settings.REFRESH_TOKEN_EXPIRE_MINUTES,
+            "refresh_token": user_refresh_token
+        }).get_success_response()
     
     if password and not Hash.verify_password(password, user.password):
         return CustomResponse(general_message="Invalid password.").get_failure_response()
 
-    if not otp:
-        return CustomResponse(message={"otp": "otp field is required."}).get_failure_response()
+    # if not otp:
+    #     return CustomResponse(message={"otp": "otp field is required."}).get_failure_response()
 
-    if otp != str(user.otp):
-        return CustomResponse(general_message="Invalid OTP!").get_failure_response()
+    # if otp != str(user.otp):
+        # return CustomResponse(general_message="Invalid OTP!").get_failure_response()
 
     user_access_token = JWTUtil.create_access_token({"id": user.id})
     user_refresh_token = JWTUtil.create_refresh_token({"id": user.id})
@@ -79,8 +106,8 @@ async def login(request: Request, db: SessionDep):
     }).get_success_response()
 
 
-@router.get('/get-access-token/')
-async def get_access_token(db: SessionDep, current_user: CurrentUserDep):
+# @router.get('/get-access-token/')
+# async def get_access_token(db: SessionDep, current_user: CurrentUserDep):
     user_id = current_user.get('id')
     if not (user :=  (await db.scalar(select(User).filter(User.id == user_id)))):
         return CustomResponse(general_message="User not found.").get_failure_response()
@@ -92,25 +119,25 @@ async def get_access_token(db: SessionDep, current_user: CurrentUserDep):
     }).get_success_response()
 
 
-@router.post('/generate-otp/')
-async def get_otp(request: Request, db: SessionDep):
-    try:
-        data = FormUtil.form_data_to_json(await request.form())
-    except MultipartParseError:
-        data = {}
+# @router.post('/generate-otp/')
+# async def get_otp(request: Request, db: SessionDep):
+#     try:
+#         data = FormUtil.form_data_to_json(await request.form())
+#     except MultipartParseError:
+#         data = {}
 
-    if not (user := (await db.scalar(select(User).filter(User.email == data.get('email'))))):
-        return CustomResponse(general_message="User not found.").get_failure_response()
+#     if not (user := (await db.scalar(select(User).filter(User.email == data.get('email'))))):
+#         return CustomResponse(general_message="User not found.").get_failure_response()
 
-    to_email = user.email
-    otp = Hash.generate_otp()
-    subject = f'{otp} - Login otp'
-    message = f'OTP {otp} - for login cemofficial dashboard.'
-    try:
-        user.otp = int(otp)
-        await MailUtil.email_without_attachment(to_email, subject, message)
-    except Exception as e:
-        print(e)
+#     to_email = user.email
+#     otp = Hash.generate_otp()
+#     subject = f'{otp} - Login otp'
+#     message = f'OTP {otp} - for login cemofficial dashboard.'
+#     try:
+#         user.otp = int(otp)
+#         await MailUtil.email_without_attachment(to_email, subject, message)
+#     except Exception as e:
+#         print(e)
 
-    await db.commit()
-    return CustomResponse(general_message="Your OTP is sent to your email!",).get_success_response()
+#     await db.commit()
+#     return CustomResponse(general_message="Your OTP is sent to your email!",).get_success_response()
--- migrations/latest.sql
+++ migrations/latest.sql
@@ -8,3 +8,17 @@ CREATE TABLE "user"
     otp        INTEGER                   NULL,
     created_at TIMESTAMP WITH TIME ZONE  NOT NULL
 );
+
+CREATE TABLE system_setting
+(
+    key        VARCHAR(100) PRIMARY KEY NOT NULL,
+    value      VARCHAR(100)             NOT NULL,
+    updated_at TIMESTAMP WITH TIME ZONE NOT NULL,
+    created_at TIMESTAMP WITH TIME ZONE NOT NULL
+);
+
+INSERT INTO "user" (id, name, password, email, role, created_at)
+VALUES ('1bb52efb-d21e-4b2b-a31c-9a405adc8f9f', 'System Admin', null, 'systemadmin@leaflog.in', 'Owner', CURRENT_TIMESTAMP);
+
+INSERT INTO system_setting (key, value, updated_at, created_at)
+VALUES ('db.version', '0.0.0', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
